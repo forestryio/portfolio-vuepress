f50d76adb941f570829543a5914241635f06420c
Nichlas W. Andersen <nichlas@itsnwa.com>
2018-08-10 18:15:20 +0200

    Added mobile navigation

--- portfolio/.vuepress/components/Navbar.vue
+++ portfolio/.vuepress/components/Navbar.vue
@@ -9,7 +9,7 @@
     }"
   >
 
-    <nav v-if="navLinks" class="navigation left">
+    <nav v-if="navLinks" class="navigation left desktop-nav">
       <ul>
         <router-link
           v-for="nav in navLinks"
@@ -28,16 +28,18 @@
     </nav>
 
     <div class="brand">
-      <div
-        v-if="logo"
-        class="logo"
-        :style="{ backgroundImage: `url(${logo})`}"
-        :title="$site.title"
-      />
-      <span v-else>{{ $site.title }}</span>
+      <router-link to="/">
+        <div
+          v-if="logo"
+          class="logo"
+          :style="{ backgroundImage: `url(${logo})`}"
+          :title="$site.title"
+        />
+        <span v-else>{{ $site.title }}</span>
+      </router-link>
     </div>
 
-    <nav v-if="navLinks" class="navigation right">
+    <nav v-if="navLinks" class="navigation right desktop-nav">
       <ul>
         <router-link
           v-for="nav in navLinks"
@@ -55,6 +57,28 @@
       </ul>
     </nav>
 
+    <div class="mobile-nav-toggle" @click="toggleMobileNav" />
+    <div class="mobile-nav" :class="{'mobile-nav--active': mobileNavActive}">
+      <nav>
+        <ul @click="toggleMobileNav">
+          <router-link
+            v-for="nav in navLinks"
+            :key="nav.text"
+            v-if="!nav.external"
+            tag="li"
+            :to="nav.link"
+            active-class="active"
+            v-text="nav.text"
+            exact
+          />
+          <li v-for="nav in navLinks" v-if="nav.external" @click="toggleMobileNav">
+            <a :href="nav.link" target="_blank">{{ nav.text }}</a>
+          </li>
+        </ul>
+        <div class="mobile-nav-close" @click="toggleMobileNav" />
+      </nav>
+    </div>
+
   </header>
 </template>
 
@@ -70,10 +94,20 @@
         required: false,
       }
     },
+    data() {
+      return {
+        mobileNavActive: false
+      }
+    },
     computed: {
       navLinks() {
         return this.$site.themeConfig.nav
       },
+    },
+    methods: {
+      toggleMobileNav() {
+        this.mobileNavActive = !this.mobileNavActive
+      }
     }
   }
 </script>
@@ -85,6 +119,7 @@
     position: relative;
     align-items: center;
     justify-content: space-between;
+    height: 6rem;
     padding: 5vw;
     font-size: 0.8rem;
     font-weight: 600;
@@ -131,5 +166,87 @@
 
   a:active { color: inherit; }
   a:visited { color: inherit; }
+
+  .desktop-nav {
+    display: none;
+  }
+
+  .mobile-nav {
+    display: block;
+    position: absolute;
+    background: #ffffff;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    padding: 2rem;
+    transform: translateY(-100%);
+    transition: transform 0.35s ease-in-out;
+    text-align: center;
+    font-size: 2rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    line-height: 2;
+  }
+
+  .mobile-nav li {
+    list-style: none;
+    cursor: pointer;
+    transition: opacity 0.15s;
+  }
+
+  .mobile-nav li:hover {
+    opacity: 0.6;
+  }
+
+  .mobile-nav--active {
+    transform: translateY(0);
+  }
+
+  .mobile-nav-close {
+    position: absolute;
+    content: '';
+    right: 0;
+    top: 0;
+    width: 3rem;
+    height: 3rem;
+    padding: 3rem;
+    background-image: url('/close.svg');
+    background-position: center;
+    background-size: 1.5rem;
+    background-repeat: no-repeat;
+    transition: opacity 0.15s;
+    cursor: pointer;
+  }
+
+  .mobile-nav-toggle {
+    display: block;
+    width: 3rem;
+    height: 3rem;
+    background-image: url('/burger.svg');
+    background-position: center;
+    background-size: 1.5rem;
+    background-repeat: no-repeat;
+    transition: opacity 0.15s;
+    cursor: pointer;
+  }
+
+  .mobile-nav-toggle:hover,
+  .mobile-nav-close:hover {
+    opacity: 0.6;
+  }
+
+  @media screen and (min-width: 600px) {
+    .desktop-nav {
+      display: block;
+    }
+    .mobile-nav-toggle {
+      display: none;
+    }
+    .mobile-nav {
+      display: none;
+    }
+  }
 
 </style>
